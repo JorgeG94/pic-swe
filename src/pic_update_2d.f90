e589cce2958da552ac8986a15f6b66f14ec0e5ca
Jorge Galvez Vallejo <jorgegalvez1694@gmail.com>
2025-07-17 09:45:03 +1000

    more updates

--- src/pic_update_2d.f90
+++ src/pic_update_2d.f90
@@ -13,7 +13,9 @@ module pic_update_2d
     real(dp), intent(in) :: flux_y_h(:,:), flux_y_hu(:,:), flux_y_hv(:,:)
     real(dp), intent(in) :: dt
 
-    integer :: i, j, nx, ny
+    integer :: i, j, nx, ny, ii, jj, i_loc, j_loc 
+    integer, parameter :: bx = 32, by = 32
+    real(dp) :: height(bx, by), x_mom(bx, by), y_mom(bx,by)
     real(dp) :: dx, dy
 
     nx = state%grid%nx
@@ -21,22 +23,43 @@ module pic_update_2d
     dx = state%grid%dx
     dy = state%grid%dy
 
-    !do concurrent (j = 1:ny, i = 1:nx)
-    do j =1, ny
-    do i = 1, nx
-      state%water_height(i,j) = state%water_height(i,j) &
-        - dt/dx * (flux_x_h(i+1,j) - flux_x_h(i,j)) &
-        - dt/dy * (flux_y_h(i,j+1) - flux_y_h(i,j))
+!    do concurrent (j = 1:ny, i = 1:nx)
+    !$omp parallel do collapse(2) 
+    do jj = 1, ny-1, by
+  do ii = 1, nx-1, bx
 
-      state%x_momentum(i,j) = state%x_momentum(i,j) &
-        - dt/dx * (flux_x_hu(i+1,j) - flux_x_hu(i,j)) &
-        - dt/dy * (flux_y_hu(i,j+1) - flux_y_hu(i,j))
+    ! Compute flux updates into local buffer
+    do j_loc = 1, min(by, ny-1 - jj + 1)
+      j = jj + j_loc - 1
+      do i_loc = 1, min(bx, nx-1 - ii + 1)
+        i = ii + i_loc - 1
 
-      state%y_momentum(i,j) = state%y_momentum(i,j) &
-        - dt/dx * (flux_x_hv(i+1,j) - flux_x_hv(i,j)) &
-        - dt/dy * (flux_y_hv(i,j+1) - flux_y_hv(i,j))
+        height(i_loc,j_loc) = - dt/dx * (flux_x_h(i+1,j) - flux_x_h(i,j)) &
+                              - dt/dy * (flux_y_h(i,j+1) - flux_y_h(i,j))
+
+        x_mom(i_loc,j_loc) = - dt/dx * (flux_x_hu(i+1,j) - flux_x_hu(i,j)) &
+                             - dt/dy * (flux_y_hu(i,j+1) - flux_y_hu(i,j))
+
+        y_mom(i_loc,j_loc) = - dt/dx * (flux_x_hv(i+1,j) - flux_x_hv(i,j)) &
+                             - dt/dy * (flux_y_hv(i,j+1) - flux_y_hv(i,j))
+      end do
     end do
+
+    ! Write once per cell from local buffers
+    do j_loc = 1, min(by, ny-1 - jj + 1)
+      j = jj + j_loc - 1
+      do i_loc = 1, min(bx, nx-1 - ii + 1)
+        i = ii + i_loc - 1
+
+        state%water_height(i,j) = state%water_height(i,j) + height(i_loc,j_loc)
+        state%x_momentum(i,j)   = state%x_momentum(i,j)   + x_mom(i_loc,j_loc)
+        state%y_momentum(i,j)   = state%y_momentum(i,j)   + y_mom(i_loc,j_loc)
+      end do
     end do
+
+  end do
+end do
+    !$omp end parallel do
   end subroutine update_state
 
 subroutine enforce_min_height(state, h_min)
