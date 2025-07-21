f191553a4368a5d084f78a2ad384d747fb7be361
Jorge Galvez Vallejo <jorgegalvez1694@gmail.com>
2025-07-21 13:36:55 +1000

    use a struct for the fluxes, like a civilised person

--- src/pic_flux_2d.f90
+++ src/pic_flux_2d.f90
@@ -4,12 +4,70 @@ module pic_flux_2d
    use local_pic_constants, only: gravity, epsilon
    implicit none
 
+  type :: flux_type 
+
+  real(dp), allocatable :: flux_h(:,:)
+  real(dp), allocatable :: flux_hu(:,:)
+  real(dp), allocatable :: flux_hv(:,:)
+
+  contains 
+
+  procedure :: allocate_fluxes
+  procedure :: set_fluxes
+  procedure :: deallocate_fluxes
+
+  end type flux_type 
+
 contains
 
-   subroutine compute_rusanov_fluxes_xy(state, flux_x_h, flux_x_hu, flux_x_hv, flux_y_h, flux_y_hu, flux_y_hv)
+  subroutine allocate_fluxes(fluxes, nx, ny)
+    class(flux_type), intent(inout) :: fluxes 
+    integer(default_int), intent(in) :: nx, ny 
+
+    allocate(fluxes%flux_h(nx,ny))
+    allocate(fluxes%flux_hu(nx,ny))
+    allocate(fluxes%flux_hv(nx,ny))
+
+  end subroutine allocate_fluxes
+
+  subroutine set_fluxes(fluxes, a)
+    class(flux_type), intent(inout) :: fluxes 
+    real(dp), intent(in) :: a 
+    !integer(int64) :: nx, ny
+    !integer(int64) :: i, j
+
+    fluxes%flux_h = a
+    fluxes%flux_hu = a
+    fluxes%flux_hv = a
+    ! in theory this could be good for the GPU
+    !nx = size(fluxes%flux_h,1)
+    !ny = size(fluxes%flux_h,2)
+    !!$omp parallel do collapse(2)
+    !do i = 1, nx
+    !  do j = 1, ny 
+    !    fluxes%flux_h(i,j) = a
+    !    fluxes%flux_hu(i,j) = a
+    !    fluxes%flux_hv(i,j) = a
+    !  end do 
+    !end do 
+    !!$omp end parallel do
+
+  end subroutine set_fluxes
+
+  subroutine deallocate_fluxes(fluxes)
+    class(flux_type), intent(inout) :: fluxes 
+
+    deallocate(fluxes%flux_h)
+    deallocate(fluxes%flux_hu)
+    deallocate(fluxes%flux_hv)
+  end subroutine deallocate_fluxes
+
+  ! subroutine compute_rusanov_fluxes_xy(state, flux_x_h, flux_x_hu, flux_x_hv, flux_y_h, flux_y_hu, flux_y_hv)
+  subroutine compute_rusanov_fluxes_xy(state, flux_x, flux_y)
       type(state_2d_type), intent(in) :: state
-      real(dp), allocatable, intent(out) :: flux_x_h(:, :), flux_x_hu(:, :), &
-                                            flux_x_hv(:, :), flux_y_h(:, :), flux_y_hu(:, :), flux_y_hv(:, :)
+      type(flux_type), intent(inout) :: flux_x, flux_y
+      !real(dp), allocatable, intent(inout) :: flux_x_h(:, :), flux_x_hu(:, :), &
+      !                                      flux_x_hv(:, :), flux_y_h(:, :), flux_y_hu(:, :), flux_y_hv(:, :)
 
       workspace: block
 
@@ -18,8 +76,8 @@ contains
          nx = state%grid%nx
          ny = state%grid%ny
 
-         allocate (flux_x_h(nx + 1, ny), flux_x_hu(nx + 1, ny), flux_x_hv(nx + 1, ny))
-         allocate (flux_y_h(nx, ny + 1), flux_y_hu(nx, ny + 1), flux_y_hv(nx, ny + 1))
+         !allocate (flux_x_h(nx + 1, ny), flux_x_hu(nx + 1, ny), flux_x_hv(nx + 1, ny))
+         !allocate (flux_y_h(nx, ny + 1), flux_y_hu(nx, ny + 1), flux_y_hv(nx, ny + 1))
 
          flux_loop: block
             real(dp) :: h_L, hu_L, hv_L, u_L, v_L, c_L
@@ -59,9 +117,9 @@ contains
                         flux(2) = 0.5_dp*(flux_L(2) + flux_R(2)) - 0.5_dp*a_max*(hu_R - hu_L)
                         flux(3) = 0.5_dp*(flux_L(3) + flux_R(3)) - 0.5_dp*a_max*(hv_R - hv_L)
 
-                        flux_x_h(i + 1, j) = flux(1)
-                        flux_x_hu(i + 1, j) = flux(2)
-                        flux_x_hv(i + 1, j) = flux(3)
+                        flux_x%flux_h(i + 1, j) = flux(1)
+                        flux_x%flux_hu(i + 1, j) = flux(2)
+                        flux_x%flux_hv(i + 1, j) = flux(3)
 
                         !end block x_flux
 
@@ -89,9 +147,9 @@ contains
                         flux(2) = 0.5_dp*(flux_L(2) + flux_R(2)) - 0.5_dp*a_max*(hu_R - hu_L)
                         flux(3) = 0.5_dp*(flux_L(3) + flux_R(3)) - 0.5_dp*a_max*(hv_R - hv_L)
 
-                        flux_y_h(i, j + 1) = flux(1)
-                        flux_y_hu(i, j + 1) = flux(2)
-                        flux_y_hv(i, j + 1) = flux(3)
+                        flux_y%flux_h(i, j + 1) = flux(1)
+                        flux_y%flux_hu(i, j + 1) = flux(2)
+                        flux_y%flux_hv(i, j + 1) = flux(3)
 
 !              end block y_flux
 
@@ -107,13 +165,13 @@ contains
 
          end block flux_loop
 
-         flux_y_h(:, 1) = 0.0_dp; flux_y_h(:, ny + 1) = 0.0_dp
-         flux_y_hu(:, 1) = 0.0_dp; flux_y_hu(:, ny + 1) = 0.0_dp
-         flux_y_hv(:, 1) = 0.0_dp; flux_y_hv(:, ny + 1) = 0.0_dp
+         flux_y%flux_h(:, 1) = 0.0_dp;  flux_y%flux_h(:, ny + 1) = 0.0_dp
+         flux_y%flux_hu(:, 1) = 0.0_dp; flux_y%flux_hu(:, ny + 1) = 0.0_dp
+         flux_y%flux_hv(:, 1) = 0.0_dp; flux_y%flux_hv(:, ny + 1) = 0.0_dp
 
-         flux_x_h(1, :) = 0.0_dp; flux_x_h(nx + 1, :) = 0.0_dp
-         flux_x_hu(1, :) = 0.0_dp; flux_x_hu(nx + 1, :) = 0.0_dp
-         flux_x_hv(1, :) = 0.0_dp; flux_x_hv(nx + 1, :) = 0.0_dp
+         flux_x%flux_h(1, :) = 0.0_dp;  flux_x%flux_h(nx + 1, :) = 0.0_dp
+         flux_x%flux_hu(1, :) = 0.0_dp; flux_x%flux_hu(nx + 1, :) = 0.0_dp
+         flux_x%flux_hv(1, :) = 0.0_dp; flux_x%flux_hv(nx + 1, :) = 0.0_dp
 
       end block workspace
    contains
@@ -134,186 +192,4 @@ contains
       end subroutine compute_velocity
    end subroutine compute_rusanov_fluxes_xy
 
-!   subroutine compute_rusanov_flux_x(state, flux_h, flux_hu, flux_hv)
-!      type(state_2d_type), intent(in) :: state
-!      real(dp), allocatable, intent(out) :: flux_h(:,:), flux_hu(:,:), flux_hv(:,:)
-!
-!      workspace: block
-!
-!         integer(default_int) :: nx, ny
-!
-!         nx = state%grid%nx
-!         ny = state%grid%ny
-!
-!         allocate(flux_h(nx+1, ny), flux_hu(nx+1,ny), flux_hv(nx+1,ny))
-!         flux_loop: block
-!            real(dp) :: h_L, hu_L, hv_L, u_L, v_L, c_L
-!            real(dp) :: h_R, hu_R, hv_R, u_R, v_R, c_R
-!            real(dp) :: a_max
-!            real(dp) :: flux_L(3), flux_R(3), flux(3)
-!            integer(default_int) :: i,j
-!!    do concurrent (j = 1:ny, i = 1:nx-1)
-!!$omp parallel do simd collapse(2) default(shared) schedule(static) private(h_L, hu_L, hv_L) &
-!!$omp private(h_R, hu_R, hv_R, u_L, v_L, u_R, v_R, flux_L, flux_R, c_L, c_R, a_max, flux)
-!            do j = 1, ny
-!               do i = 1, nx-1
-!
-!                  h_L = state%water_height(i,j)
-!                  hu_L = state%x_momentum(i,j)
-!                  hv_L = state%y_momentum(i,j)
-!
-!                  h_R = state%water_height(i+1,j)
-!                  hu_R = state%x_momentum(i+1,j)
-!                  hv_R = state%y_momentum(i+1,j)
-!
-!                  ! if(h_L < epsilon .and. h_R < epsilon) then
-!                  !    flux_h(i+1, j) = 0.0_dp
-!                  !    flux_hu(i+1, j) = 0.0_dp
-!                  !    flux_hv(i+1, j) = 0.0_dp
-!                  ! else
-!                  if (h_L < epsilon) h_L = epsilon
-!                  if (h_R < epsilon) h_R = epsilon
-!                  if (h_L > epsilon) then
-!                    u_L = hu_L / h_L
-!                    v_L = hv_L / h_L
-!                  else
-!                    u_L = 0.0_dp
-!                    v_L = 0.0_dp
-!                  end if
-!                  if (h_R > epsilon) then
-!                    u_R = hu_R / h_R
-!                    v_R = hv_R / h_R
-!                  else
-!                    u_R = 0.0_dp
-!                    v_R = 0.0_dp
-!                  end if
-!
-!                  flux_L = [ hu_L, hu_L*u_L + 0.5_dp * gravity * h_L**2, hu_L*v_L]
-!                  flux_R = [ hu_R, hu_R*u_R + 0.5_dp * gravity * h_R**2, hu_R*v_R]
-!
-!                  c_L = abs(u_L) + sqrt(gravity * h_L)
-!                  c_R = abs(u_R) + sqrt(gravity * h_R)
-!
-!                  a_max = max(c_L, c_R)
-!
-!                  !flux = 0.5_dp * (flux_L + flux_R) - 0.5_dp * a_max * ([h_R, hu_R, hv_R] - [h_L, hu_L, hv_L])
-!                  flux(1) = 0.5_dp * (flux_L(1) + flux_R(1)) - 0.5_dp * a_max * (h_R  - h_L)
-!                  flux(2) = 0.5_dp * (flux_L(2) + flux_R(2)) - 0.5_dp * a_max * (hu_R - hu_L)
-!                  flux(3) = 0.5_dp * (flux_L(3) + flux_R(3)) - 0.5_dp * a_max * (hv_R - hv_L)
-!
-!                  flux_h(i+1,j) = flux(1)
-!                  flux_hu(i+1,j) = flux(2)
-!                  flux_hv(i+1,j) = flux(3)
-!                  ! endif
-!
-!               end do
-!            end do
-!            !$omp end parallel do simd
-!         end block flux_loop
-!
-!
-!         flux_h(1,:) = 0.0_dp;    flux_h(nx+1,:) = 0.0_dp
-!         flux_hu(1,:) = 0.0_dp;   flux_hu(nx+1,:) = 0.0_dp
-!         flux_hv(1,:) = 0.0_dp;   flux_hv(nx+1,:) = 0.0_dp
-!
-!      end block workspace
-!
-!   end subroutine compute_rusanov_flux_x
-!
-!   subroutine compute_rusanov_flux_y(state, flux_h, flux_hu, flux_hv)
-!      type(state_2d_type), intent(in) :: state
-!      real(dp), allocatable, intent(out) :: flux_h(:,:), flux_hu(:,:), flux_hv(:,:)
-!
-!      workspace: block
-!
-!         integer(default_int) :: nx, ny
-!
-!         nx = state%grid%nx
-!         ny = state%grid%ny
-!
-!         allocate(flux_h(nx, ny+1), flux_hu(nx,ny+1), flux_hv(nx,ny+1))
-!         flux_loop: block
-!            real(dp) :: h_L, hu_L, hv_L, u_L, v_L, c_L
-!            real(dp) :: h_R, hu_R, hv_R, u_R, v_R, c_R
-!            real(dp) :: a_max
-!            integer(default_int) :: i,j
-!            real(dp) :: flux_L(3), flux_R(3), flux(3)
-!            real(dp) :: sqroot_gravity
-!            sqroot_gravity = sqrt(gravity)
-!            !do concurrent (j = 1:ny-1, i = 1:nx)
-!            !$omp parallel do simd collapse(2) schedule(static) default(shared) private( h_L, hu_L, hv_L) &
-!            !$omp private(h_R, hu_R, hv_R, u_L, v_L, u_R, v_R, flux_L, flux_R, c_L, c_R, a_max, flux)
-!            do j = 1, ny - 1
-!               do i = 1, nx
-!                  !do j = 1, ny
-!                  !do i = 1, nx
-!
-!                  h_L = state%water_height(i,j)
-!                  hu_L = state%x_momentum(i,j)
-!                  hv_L = state%y_momentum(i,j)
-!
-!                  h_R = state%water_height(i,j+1)
-!                  hu_R = state%x_momentum(i,j+1)
-!                  hv_R = state%y_momentum(i,j+1)
-!
-!                  ! if(h_L < epsilon .and. h_R < epsilon) then
-!                  !    flux_h(i, j+1) = 0.0_dp
-!                  !    flux_hu(i, j+1) = 0.0_dp
-!                  !    flux_hv(i, j+1) = 0.0_dp
-!                  ! else
-!                  if (h_L < epsilon) h_L = epsilon
-!                  if (h_R < epsilon) h_R = epsilon
-!
-!                  if (h_L > epsilon) then
-!                    u_L = hu_L / h_L
-!                    v_L = hv_L / h_L
-!                  else
-!                    u_L = 0.0_dp
-!                    v_L = 0.0_dp
-!                  end if
-!                  if (h_R > epsilon) then
-!                    u_R = hu_R / h_R
-!                    v_R = hv_R / h_R
-!                  else
-!                    u_R = 0.0_dp
-!                    v_R = 0.0_dp
-!                  end if
-!                  !u_L = merge(hu_L / h_L, 0.0_dp, h_L > epsilon)
-!                  !v_L = merge(hv_L / h_L, 0.0_dp, h_L > epsilon)
-!                  !u_R = merge(hu_R / h_R, 0.0_dp, h_R > epsilon)
-!                  !v_R = merge(hv_R / h_R, 0.0_dp, h_R > epsilon)
-!
-!
-!                  flux_L = [ hu_L, hu_L*u_L + 0.5_dp * gravity * h_L**2, hu_L*v_L]
-!                  flux_R = [ hu_R, hu_R*u_R + 0.5_dp * gravity * h_R**2, hu_R*v_R]
-!
-!                  c_L = abs(u_L) + sqroot_gravity * sqrt(h_L)
-!                  c_R = abs(u_R) + sqroot_gravity * sqrt(h_R)
-!
-!                  a_max = max(c_L, c_R)
-!
-!                  !flux = 0.5_dp * (flux_L + flux_R) - 0.5_dp * a_max * ([h_R, hu_R, hv_R] - [h_L, hu_L, hv_L])
-!                  flux(1) = 0.5_dp * (flux_L(1) + flux_R(1)) - 0.5_dp * a_max * (h_R  - h_L)
-!                  flux(2) = 0.5_dp * (flux_L(2) + flux_R(2)) - 0.5_dp * a_max * (hu_R - hu_L)
-!                  flux(3) = 0.5_dp * (flux_L(3) + flux_R(3)) - 0.5_dp * a_max * (hv_R - hv_L)
-!
-!
-!                  flux_h(i,j+1) = flux(1)
-!                  flux_hu(i,j+1) = flux(2)
-!                  flux_hv(i,j+1) = flux(3)
-!                  ! endif
-!
-!               end do
-!            end do
-!            !$omp end parallel do simd
-!         end block flux_loop
-!
-!
-!         flux_h(:,1) = 0.0_dp;    flux_h(:,ny+1) = 0.0_dp
-!         flux_hu(:,1) = 0.0_dp;   flux_hu(:,ny+1) = 0.0_dp
-!         flux_hv(:,1) = 0.0_dp;   flux_hv(:,ny+1) = 0.0_dp
-!
-!      end block workspace
-!
-!   end subroutine compute_rusanov_flux_y
 end module pic_flux_2d
--- test/test_fluxes.f90
+++ test/test_fluxes.f90
@@ -3,7 +3,7 @@ module test_fluxes
    use pic_matrix_printer, only: print_array
    use pic_string_utils, only: to_string
    use pic_types, only: dp
-   use pic_flux_2d, only: compute_rusanov_fluxes_xy
+   use pic_flux_2d, only: compute_rusanov_fluxes_xy, flux_type
    use pic_grid_2d, only: grid_2d_type, init_grid
    use pic_state_2d, only: state_2d_type, initialize_state, initialize_dam_break
    use pic_boundaries, only: apply_reflective_boundaries
@@ -32,8 +32,8 @@ contains
       type(state_2d_type) :: state
       real(dp), parameter :: expected_dt = 0.11288091024643272_dp
       real(dp) :: cfl, dt
-      real(dp), allocatable :: flux_x_h(:, :), flux_x_hu(:, :), flux_x_hv(:, :)
-      real(dp), allocatable :: flux_y_h(:, :), flux_y_hu(:, :), flux_y_hv(:, :)
+      type(flux_type) :: flux_x, flux_y
+
       real(dp), parameter :: flux_x_h_ref(4, 2) = reshape([ &
                             0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp &
                                                           ], [4, 2])
@@ -77,24 +77,29 @@ contains
       call check(error, is_equal(dt, expected_dt), "Time step should be "//to_string(expected_dt))
       call apply_reflective_boundaries(state)
 
-      allocate (flux_x_h(nx + 1, ny), flux_x_hu(nx + 1, ny), flux_x_hv(nx + 1, ny))
-      allocate (flux_y_h(nx, ny + 1), flux_y_hu(nx, ny + 1), flux_y_hv(nx, ny + 1))
 
-      flux_x_h = 0.0_dp
-      flux_x_hu = 0.0_dp
-      flux_x_hv = 0.0_dp
-      flux_y_h = 0.0_dp
-      flux_y_hu = 0.0_dp
-      flux_y_hv = 0.0_dp
+      call flux_x%allocate_fluxes(nx + 1, ny)
+      call flux_y%allocate_fluxes(nx, ny + 1)
+      call flux_x%set_fluxes(0.0_dp)
+      call flux_y%set_fluxes(0.0_dp)
 
-      call compute_rusanov_fluxes_xy(state, flux_x_h, flux_x_hu, flux_x_hv, &
-                                     flux_y_h, flux_y_hu, flux_y_hv)
-      call check(error, all(is_equal(flux_x_h, flux_x_h_ref)), "flux_x_h should match reference")
-      call check(error, all(is_equal(flux_x_hu, flux_x_hu_ref)), "flux_x_hu should match reference")
-      call check(error, all(is_equal(flux_x_hv, flux_x_hv_ref)), "flux_x_hv should match reference")
-      call check(error, all(is_equal(flux_y_h, flux_y_h_ref)), "flux_y_h should match reference")
-      call check(error, all(is_equal(flux_y_hu, flux_y_hu_ref)), "flux_y_hu should match reference")
-      call check(error, all(is_equal(flux_y_hv, flux_y_hv_ref)), "flux_y_hv should match reference")
+      ! flux_x_h = 0.0_dp
+      ! flux_x_hu = 0.0_dp
+      ! flux_x_hv = 0.0_dp
+      ! flux_y_h = 0.0_dp
+      ! flux_y_hu = 0.0_dp
+      ! flux_y_hv = 0.0_dp
+
+      call compute_rusanov_fluxes_xy(state, flux_x, flux_y)
+      call check(error, all(is_equal(flux_x%flux_h, flux_x_h_ref)), "flux_x_h should match reference")
+      call check(error, all(is_equal(flux_x%flux_hu, flux_x_hu_ref)), "flux_x_hu should match reference")
+      call check(error, all(is_equal(flux_x%flux_hv, flux_x_hv_ref)), "flux_x_hv should match reference")
+      call check(error, all(is_equal(flux_y%flux_h, flux_y_h_ref)), "flux_y_h should match reference")
+      call check(error, all(is_equal(flux_y%flux_hu, flux_y_hu_ref)), "flux_y_hu should match reference")
+      call check(error, all(is_equal(flux_y%flux_hv, flux_y_hv_ref)), "flux_y_hv should match reference")
+
+      call flux_x%deallocate_fluxes()
+      call flux_y%deallocate_fluxes()
 
    end subroutine test_compute_rusanov_fluxes
 
@@ -105,8 +110,7 @@ contains
       type(state_2d_type) :: state
       real(dp), parameter :: expected_dt = 0.11288091024643272_dp
       real(dp) :: cfl, dt
-      real(dp), allocatable :: flux_x_h(:, :), flux_x_hu(:, :), flux_x_hv(:, :)
-      real(dp), allocatable :: flux_y_h(:, :), flux_y_hu(:, :), flux_y_hv(:, :)
+      type(flux_type) :: flux_x, flux_y
       real(dp), parameter :: flux_x_h_ref(4, 2) = reshape([ &
                             0.000000_dp, 5.000000_dp, 5.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp, 0.000000_dp &
                                                           ], [4, 2])
@@ -151,15 +155,10 @@ contains
       call check(error, is_equal(dt, expected_dt), "Time step should be "//to_string(expected_dt))
       call apply_reflective_boundaries(state)
 
-      allocate (flux_x_h(nx + 1, ny), flux_x_hu(nx + 1, ny), flux_x_hv(nx + 1, ny))
-      allocate (flux_y_h(nx, ny + 1), flux_y_hu(nx, ny + 1), flux_y_hv(nx, ny + 1))
-
-      flux_x_h = 0.0_dp
-      flux_x_hu = 0.0_dp
-      flux_x_hv = 0.0_dp
-      flux_y_h = 0.0_dp
-      flux_y_hu = 0.0_dp
-      flux_y_hv = 0.0_dp
+      call flux_x%allocate_fluxes(nx + 1, ny)
+      call flux_y%allocate_fluxes(nx, ny + 1)
+      call flux_x%set_fluxes(0.0_dp)
+      call flux_y%set_fluxes(0.0_dp)
 
       block
          integer :: i, j
@@ -171,14 +170,17 @@ contains
          end do
       end block
 
-      call compute_rusanov_fluxes_xy(state, flux_x_h, flux_x_hu, flux_x_hv, &
-                                     flux_y_h, flux_y_hu, flux_y_hv)
-      call check(error, all(is_equal(flux_x_h, flux_x_h_ref)), "flux_x_h should match reference")
-      call check(error, all(is_equal(flux_x_hu, flux_x_hu_ref)), "flux_x_hu should match reference")
-      call check(error, all(is_equal(flux_x_hv, flux_x_hv_ref)), "flux_x_hv should match reference")
-      call check(error, all(is_equal(flux_y_h, flux_y_h_ref)), "flux_y_h should match reference")
-      call check(error, all(is_equal(flux_y_hu, flux_y_hu_ref)), "flux_y_hu should match reference")
-      call check(error, all(is_equal(flux_y_hv, flux_y_hv_ref)), "flux_y_hv should match reference")
+      call compute_rusanov_fluxes_xy(state, flux_x, flux_y)
+
+      call check(error, all(is_equal(flux_x%flux_h, flux_x_h_ref)), "flux_x_h should match reference")
+      call check(error, all(is_equal(flux_x%flux_hu, flux_x_hu_ref)), "flux_x_hu should match reference")
+      call check(error, all(is_equal(flux_x%flux_hv, flux_x_hv_ref)), "flux_x_hv should match reference")
+      call check(error, all(is_equal(flux_y%flux_h, flux_y_h_ref)), "flux_y_h should match reference")
+      call check(error, all(is_equal(flux_y%flux_hu, flux_y_hu_ref)), "flux_y_hu should match reference")
+      call check(error, all(is_equal(flux_y%flux_hv, flux_y_hv_ref)), "flux_y_hv should match reference")
+
+      call flux_x%deallocate_fluxes()
+      call flux_y%deallocate_fluxes()
 
    end subroutine test_compute_rusanov_fluxes_nonzero
 
