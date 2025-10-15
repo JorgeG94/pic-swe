ba90750324996c63f2f6bc3a95b0d1906e6bc649
Jorge Galvez Vallejo <jorgegalvez1694@gmail.com>
2025-10-15 09:10:19 +0800

    main.f90

---
+++ app/main.f90
@@ -0,0 +1,61 @@
+program main
+   use pic, only: pic_print_banner
+   use pic_types, only: sp, dp, default_int
+   use pic_timer, only: pic_timer_type
+   use pic_swe_grid_2d
+   use pic_swe_state_2d
+   use pic_swe_time_driver
+   use pic_logger, only: global => global_logger, info_level
+   implicit none
+   real(dp), dimension(:, :), allocatable :: A, B, C
+   integer(default_int) :: n, m, k
+   real(dp) :: xmin, xmax, ymin, ymax, dx, dy
+   real(dp), parameter :: zero = 0.0_dp
+   type(grid_2d_type) :: grid
+
+   call pic_print_banner()
+   call global%configure(info_level)
+
+   xmin = zero
+   ymin = zero
+   xmax = 1024.0_dp
+   ymax = 1024.0_dp
+   dx = 0.25_dp
+   block
+      type(pic_timer_type) :: my_timer
+      real(dp) :: elapsed_time
+      type(state_2d_type) :: state
+      real(dp) :: t_end
+      real(dp) :: cfl
+      real(dp), parameter :: h_left = 20.0_dp
+      real(dp), parameter :: h_right = 7.0_dp
+      real(dp), parameter :: x_dplit = 150.0_dp
+
+      t_end = 1.05_dp
+      cfl = 0.45_dp
+      ! both the grid and the initialization can be parallelized
+      call my_timer%start()
+      call init_grid(grid, xmin, xmax, ymin, ymax, dx)
+      call generate_2d_grids(grid)
+      call my_timer%stop()
+      elapsed_time = my_timer%get_elapsed_time()
+      call global%info("Grid generation tok "//to_string(elapsed_time)//" seconds")
+      call global%info("Grid size: nx = "//to_string(grid%nx)//" ny = "//to_string(grid%ny))
+      call global%info("Total number of points is "//to_string(grid%nx*grid%ny))
+      call my_timer%start()
+      call state%initialize_state(grid)
+      call global%info("Dam break initialized with h_left = "//to_string(h_left)// &
+                       ", h_right = "//to_string(h_right)//", x_dplit = "//to_string(x_dplit))
+      call initialize_dam_break(state, h_left, h_right, x_dplit)
+      call my_timer%stop()
+      elapsed_time = my_timer%get_elapsed_time()
+      call global%info("Grid initialization tok "//to_string(elapsed_time)//" seconds")
+
+      call my_timer%start()
+      call time_loop(state, t_end, cfl)
+      call my_timer%stop()
+      elapsed_time = my_timer%get_elapsed_time()
+
+      call global%info("Time loop took "//to_string(elapsed_time)//" seconds")
+   end block
+end program main
